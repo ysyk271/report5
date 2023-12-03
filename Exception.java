e243a6ec1976093c2e241b7a96c96a3a8f57e5f9
Takehara Yasuyuki <e225421@ie.u-ryukyu.ac.jp>
2023-12-03 14:55:57 +0900

    3rd commit

--- Exception.java
+++ Exception.java
@@ -1,13 +1,8 @@
 
-
 class Exception {
     public static void main(String[] args) {
-        try {
-            String str =null;
-            System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+            String str = "壱百満";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
     }
 }
